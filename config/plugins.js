9355bd7b95530f335a6a02ef0eb70c822c3aabda
vasim15 <72203433+vasim15@users.noreply.github.com>
2023-09-05 14:25:45 +0530

    Add files via upload

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,15 @@ module.exports = ({ env }) => ({
     "netlify-deployments": {
       enabled: true,
       config: {
-        accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN
+        accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+        sites: [
+            {
+              name: 'peaceful-sopapillas-dea225',
+              id: "1e9e7e16-76ad-4845-87a0-727f3681da69",
+              buildHook: "https://api.netlify.com/build_hooks/<hook_id>",
+            //   branch: 'master' // optional
+            }
+          ]
       },
     },
   });
