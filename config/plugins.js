4ea7151d574645665485b58bfd01b00a12d4d585
vasim15 <72203433+vasim15@users.noreply.github.com>
2023-09-29 00:22:33 +0530

    Add files via upload
    with new version

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,19 @@
 module.exports = ({ env }) => ({
-    "netlify-deployments": {
-      enabled: true,
+    // ...
+    upload: {
       config: {
-        accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
-        sites: [
-            {
-              name: 'peaceful-sopapillas-dea225',
-              id: "1e9e7e16-76ad-4845-87a0-727f3681da69",
-              buildHook: "https://api.netlify.com/build_hooks/64f6ee6df7fa1075d739452d",
-              branch: 'main' // optional
-            }
-          ]
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
       },
     },
+    // ...
   });
